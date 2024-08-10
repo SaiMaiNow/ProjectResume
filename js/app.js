3b7370174bdaf1c82c50e7567f04d66a8dfb92a5
SaiMaiNow <pword988@gmail.com>
2024-08-11 02:50:57 +0700

    this is code new version

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 function copy(text) {
     navigator.clipboard.writeText(text);
-
     Swal.fire({
         position: "center",
         icon: "success",
-        title: "Success to copy text",
+        title: "Success to copy email",
         showConfirmButton: false,
         timer: 1500
     });
+}
+
+var isNext = false;
+
+function scrollNext(id) {
+    if (!isNext) {
+        isNext = true;
+
+        $('html, body').animate({
+            scrollTop: (id == "#home") ? 0 : $(id).offset().top,
+        }, 1000, function() {
+            $(window).off('scroll');
+            isNext = false;
+        });
+    }
 }
